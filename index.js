abd63f41cc18fabf18b22171463b1153cf2c612a
TakiNouar <132835122+TakiNouar@users.noreply.github.com>
2023-11-30 18:37:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,78 +1,82 @@
-const OrderPage = document.getElementById("orderPage");
-const MainPage = document.getElementById("MainPage");
-let body = document.getElementById("body");
+const OrderPage = document.getElementById('orderPage');
+const MainPage = document.getElementById('MainPage');
+let body = document.getElementById('body');
 
-document.getElementById("btn-order").onclick = function () {
-  MainPage.classList.add("FadeOut");
-  OrderPage.classList.remove("FadeOut");
+// document.getElementById('btn-order').onclick = function () {
+//     MainPage.classList.add('FadeOut');
+//     OrderPage.classList.remove('FadeOut');
 
-  OrderPage.classList.add("FadeIn");
-  MainPage.classList.remove("FadeIn");
-  setTimeout(function () {
-    OrderPage.style.display = "flex";
-    MainPage.style.display = "none";
-  }, 495);
-};
-document.getElementById("back_1").onclick = function () {
-  MainPage.classList.remove("FadeOut");
-  OrderPage.classList.add("FadeOut");
+//     OrderPage.classList.add('FadeIn');
+//     MainPage.classList.remove('FadeIn');
+//     setTimeout(function () {
+//         OrderPage.style.display = 'flex';
+//         MainPage.style.display = 'none';
+//     }, 495);
+// };
+// document.getElementById('back_1').onclick = function () {
+//     MainPage.classList.remove('FadeOut');
+//     OrderPage.classList.add('FadeOut');
 
-  MainPage.classList.add("FadeIn");
-  OrderPage.classList.remove("FadeIn");
-  setTimeout(function () {
-    OrderPage.style.display = "none";
-    MainPage.style.display = "flex";
-  }, 500);
-};
+//     MainPage.classList.add('FadeIn');
+//     OrderPage.classList.remove('FadeIn');
+//     setTimeout(function () {
+//         OrderPage.style.display = 'none';
+//         MainPage.style.display = 'flex';
+//     }, 500);
+// };
 
 // Get all radio buttons with name "gender"
-const radios = document.getElementsByName("colors");
-let product_color = "";
+const radios = document.getElementsByName('colors');
+let product_color = '';
 
 // Loop through all radio buttons
 for (let i = 0; i < radios.length; i++) {
-  radios[i].addEventListener("click", function () {
-    for (let j = 0; j < radios.length; j++) {
-      if (radios[j] !== this) {
-        radios[j].checked = false;
-      } else if (radios[j] === this && radios[j].checked) {
-        product_color = radios[j].value;
-      }
-    }
-  });
+    radios[i].addEventListener('click', function () {
+        for (let j = 0; j < radios.length; j++) {
+            if (radios[j] !== this) {
+                radios[j].checked = false;
+            } else if (radios[j] === this && radios[j].checked) {
+                product_color = radios[j].value;
+            }
+        }
+    });
 }
-const myForm = document.getElementById("myForm");
-myForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // prevent page from reloading
-  // code to execute when form is submitted
-  try {
-    const name = document.getElementById("Name").value;
-    const Email = document.getElementById("Email").value;
-    const tel = document.getElementById("P-number").value;
-    const Wilaya = document.getElementById("W-Adress").value;
-    const Street = document.getElementById("S-Adress").value;
-    const Quantitys = document.getElementById("count");
-    const Quantity = Quantitys.value;
+const myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent page from reloading
+    // code to execute when form is submitted
+    try {
+        const name = document.getElementById('Name').value;
+        const Email = document.getElementById('Email').value;
+        const tel = document.getElementById('P-number').value;
+        const Wilaya = document.getElementById('W-Adress').value;
+        const Street = document.getElementById('S-Adress').value;
+        const City = document.getElementById('C-Adress').value;
+        const code = document.getElementById('P-code').value;
+        const Quantitys = document.getElementById('count');
+        const Quantity = Quantitys.value;
 
-    let sendToYounes = {
-      name: name,
-      from_name: name,
-      email_id: Email,
-      Wilaya: Wilaya,
-      Street: Street,
-      tel: "+213 " + tel,
-      Quantity: Quantity,
-    };
-    emailjs.send("service_tbwherh", "template_gpwghsv", sendToYounes);
-    console.log(sendToYounes);
+        let sendToYounes = {
+            code: code,
+            name: name,
+            from_name: name,
+            email_id: Email,
+            Wilaya: Wilaya,
+            City: City,
+            Street: Street,
+            tel: '+213 ' + tel,
+            Quantity: Quantity
+        };
+        emailjs.send('service_tbwherh', 'template_gpwghsv', sendToYounes);
+        console.log(sendToYounes);
 
-    console.log("Form submitted!");
-    alert("Order submitted!");
-  } catch (error) {
-    console.log(error);
-    alert(
-      "something went wrong, please try again or contact this number 0781 43 30 88"
-    );
-  }
+        console.log('Form submitted!');
+        alert('Order submitted!');
+    } catch (error) {
+        console.log(error);
+        alert(
+            'something went wrong, please try again or contact this number 0781 43 30 88'
+        );
+    }
 });
-document.getElementById("btn-order2").onclick = function () {};
+document.getElementById('btn-order2').onclick = function () {};
